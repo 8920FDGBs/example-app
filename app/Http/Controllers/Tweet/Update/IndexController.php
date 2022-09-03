c1de28e8dfb27b09b897aaadc8e4bd5ba594be3e
8920FDGBs <s_mura43107@old-fshn.com>
2022-09-03 21:19:10 +0900

    編集画面作成

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -20,6 +20,6 @@ class IndexController extends Controller
         $tweetId = (int) $request->route('tweetId');
         $tweet = Tweet::where('id', $tweetId)->firstOrFail();
 
-        dd($tweet);
+        return view('tweet.update')->with('tweet', $tweet);
     }
 }
