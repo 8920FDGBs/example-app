b9b62833a49e910f08fb43b469969c6d949b0581
8920FDGBs <s_mura43107@old-fshn.com>
2022-09-06 22:15:12 +0900

    ログアウト機能作成

--- resources/views/components/element/button-a.blade.php
+++ resources/views/components/element/button-a.blade.php
@@ -1,19 +1,19 @@
 @props([
     'href' => '',
-    'theme' => 'primary'
+    'theme' => 'primary',
 ])
 
 @php
-    if (! function_exist('getThemeClassForButtonA')) {
-        function getThemeClassForButtonA($theme)
-        {
-            return match ($theme) {
-                'primary' => 'text-white bg-blue-500 hover:bg-blue-600 focus:ring-blue-500',
-                'secondary' => 'text-white bg-red-500 hover:bg-red-600 focus:ring-red-500',
-                'default' => '',
-            }
-        }
+if (! function_exists('getThemeClassForButtonA')) {
+    function getThemeClassForButtonA($theme)
+    {
+        return match ($theme) {
+            'primary' => 'text-white bg-blue-500 hover:bg-blue-600 focus:ring-blue-500',
+            'secondary' => 'text-white bg-red-500 hover:bg-red-600 focus:ring-red-500',
+            'default' => '',
+        };
     }
+}
 @endphp
 
 <a href="{{ $href }}" class="
