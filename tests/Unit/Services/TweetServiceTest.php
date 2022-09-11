8219179532c05321a48814e524f6b79f9d22ca3b
8920FDGBs <s_mura43107@old-fshn.com>
2022-09-11 19:54:43 +0900

    checkOwnTweetのテスト作成と実施

--- tests/Unit/Services/TweetServiceTest.php
+++ tests/Unit/Services/TweetServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Services;
 
 use App\Services\TweetService;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class TweetServiceTest extends TestCase
@@ -10,11 +11,18 @@ class TweetServiceTest extends TestCase
     /**
      * A basic unit test example.
      *
+     * @runInSeparateProcess
      * @return void
      */
     public function test_check_own_tweet()
     {
         $tweetService = new TweetService();
+
+        $mock = Mockery::mock('alias:App\Models\Tweet');
+        $mock->shouldReceive('where->first')->andReturn((object)[
+            'id' => 1,
+            'user_id' => 1
+        ]);
 
         $result = $tweetService->checkOwnTweet(1, 1);
         $this->assertTrue($result);
